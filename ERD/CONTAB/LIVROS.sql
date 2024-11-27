629899bc050e9ab1880a9e9c6206f0156551b53a
Giovani Perotto Mesquita <giovanipm@gmail.com>
2024-11-26 21:44:55 -0300

    giova(LAPTOP-K5TUGPF) 241126214455

---
+++ ERD/CONTAB/LIVROS.sql
@@ -0,0 +1,3 @@
+INSERT INTO [LIVROS] ([LIVRO], [DESC], [USER], [PROG], [DATA], [HORA]) VALUES ('AA', 'LIVRO GERENCIAL', 'SQL', 'SQL', date('now'), time('now'));
+INSERT INTO [LIVROS] ([LIVRO], [DESC], [USER], [PROG], [DATA], [HORA]) VALUES ('AF', 'LIVRO FINANCEIRO', 'SQL', 'SQL', date('now'), time('now'));
+INSERT INTO [LIVROS] ([LIVRO], [DESC], [USER], [PROG], [DATA], [HORA]) VALUES ('BA', 'LIVRO ORCAMENTARIO', 'SQL', 'SQL', date('now'), time('now'));
---
+++ ERD/CONTAB/TPLOTES.sql
@@ -0,0 +1,5 @@
+INSERT INTO [TPLOTES] ([TIPOLOTE], [DESC], [USER], [PROG], [DATA], [HORA]) VALUES ('I', 'ESTOQUE', 'SQL', 'SQL', date('now'), time('now'));
+INSERT INTO [TPLOTES] ([TIPOLOTE], [DESC], [USER], [PROG], [DATA], [HORA]) VALUES ('V', 'COMPRAS', 'SQL', 'SQL', date('now'), time('now'));
+INSERT INTO [TPLOTES] ([TIPOLOTE], [DESC], [USER], [PROG], [DATA], [HORA]) VALUES ('S', 'VENDAS', 'SQL', 'SQL', date('now'), time('now'));
+INSERT INTO [TPLOTES] ([TIPOLOTE], [DESC], [USER], [PROG], [DATA], [HORA]) VALUES ('M', 'PRODUCAO', 'SQL', 'SQL', date('now'), time('now'));
+INSERT INTO [TPLOTES] ([TIPOLOTE], [DESC], [USER], [PROG], [DATA], [HORA]) VALUES ('A', 'CONTABIL', 'SQL', 'SQL', date('now'), time('now'));
